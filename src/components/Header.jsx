436731208136cd8e61c0fd741bacaf92fd58ea47
johntvale <johntvale@gmail.com>
2021-07-22 14:56:53 -0400

    Requisito 1 resolvido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,16 @@
 // implement Header component here
 //
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return ('');
+  }
+}
+
+// Header.propTypes = {
+//   text: PropTypes.string
+// };
+
+export default Header;
