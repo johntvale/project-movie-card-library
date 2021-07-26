bb3957147d7d2c7e12ffed4b7dc322f5716d4438
johntvale <johntvale@gmail.com>
2021-07-26 12:26:22 -0400

    Rating OK + correçao de lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { imagePath, title, subtitle, storyline } } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
@@ -14,6 +15,7 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,13 +5,17 @@ import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    console.log(this.props);
+    const { rating } = this.props;
     return (
-      <div>
-        hello world
+      <div className="movie-card-rating">
+        <p className="rating">{ rating }</p>
       </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
